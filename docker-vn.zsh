6d5ed33bc887b341e547e859e7af2f72f07ffb32
togatoga <togasakitogatoga@gmail.com>
2018-02-03 01:45:55 +0900

    Add docker-vn.zsh

---
+++ docker-vn.zsh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# git clone https://github.com/togatoga/vn
+# cd vn; "docker build -t vn ."
+
+function docker-build-vn() {
+	pushd
+	if [ ! -d "/tmp/vn" ]; then
+		git clone https://github.com/togatoga/vn /tmp/vn
+	fi
+	cd /tmp/vn
+	git pull origin master
+	docker build --no-cache=true -t vn .
+	popd
+}
+alias vn="docker run -it --rm vn python vn.py"
+alias vni="docker run -it --rm vn python vn.py translate --interactive"
+alias vnt="docker run -it --rm vn python vn.py translate $*"
